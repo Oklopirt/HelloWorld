432ab38f1a69855bc46d15f2761baf1c3b577848
Alexander Tripolko <tripolko@gmail.com>
2015-04-24 11:33:14 +0600

    1

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -6,19 +6,11 @@ namespace HelloWorld
     {
         public static void Main()
         {
-<<<<<<< .mine
-            Console.WriteLine("Проверка2323sdf");
-
-
-
-
-=======
-            Console.WriteLine("Ïðîâåðêàûâà");
+            Console.WriteLine("seasons");
+            Console.WriteLine("sesons2");
             foreach (vat t in new[1, 2, 3]){
                 Console.WriteLine(t);
-                
             }
->>>>>>> .theirs
         }
     }
 }
