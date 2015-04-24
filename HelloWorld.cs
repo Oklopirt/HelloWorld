94cf982135ef0f831a8036a6871252052909c534
Alexander Tripolko <tripolko@gmail.com>
2015-04-24 15:46:48 +0600

    Third commit

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
             Console.WriteLine("sesons2");
             Console.WriteLine("sesons2");
             Console.WriteLine("sesons3");
-
+            Console.WriteLine("sesons4");
             foreach (vat t in new[1, 2, 3]){
                 Console.WriteLine(t);
             }
